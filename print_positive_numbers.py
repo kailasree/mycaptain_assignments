0b5c5436b03ba48da1dd567609b4eff1ddd51487
kailasree <67535167+kailasree@users.noreply.github.com>
2023-06-24 22:38:45 +0530

    Add files via upload
    the program collects the list from the user and prints our the positive numbers from the list

---
+++ print_positive_numbers.py
@@ -0,0 +1,8 @@
+
+x=eval(input("Enter a list with positive and negative numbers: "))
+
+#x=[1,-9,89,54,-78,23,-65] 
+print("These are the positive numbers in the list")
+for i in x:
+    if i>0:
+        print(i)
